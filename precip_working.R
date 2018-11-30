7e40d52e5ab6f66df4042f57545c684f0a7277bb
Casey Lab <bnc0015@mix.wvu.edu>
2018-11-30 13:06:30 -0500

    workong on sequencing by date

--- precip_working.R
+++ precip_working.R
@@ -3,8 +3,9 @@ rm(list=ls())
 
 library(zoo)
 library(ggplot2)
+library(plyr) #load plyr first to reduce problems when running dplyr
 library(dplyr)
-library(plyr)
+
 
 Precip<-read.csv("N:/Weather_Station/Weather_Station_Data_Summary/DATA/Precip_9-22-18_2018.csv")[,1:3] #reads in Precip, excludes columns with datalogger info but no data
 head(Precip)
@@ -16,14 +17,12 @@ str(Precip)
 #Renaming columns to something meaningful
 names(Precip)[colnames(Precip)=="CR6"] <- "Precipitation" 
 names(Precip)[colnames(Precip)=="TOA5"] <- "Date_Time"
-Precip1 <- as.numeric(as.character(Precip$Precipitation))
 
-#td_precip<-as.POSIXct(Precip$TOA5,format = "%Y-%m-%d %H:%M:%S",tz="EST") #creates POSIXct object with the dates and time (30 in timestep). 
-#All times in EST. POSIXct handles dates, times, and timezones so it was selected for this exercise 
-#precip1<-as.numeric(as.vector(Precip$CR6))#create the vector with the precipitation values
-#str(td_precip)
+#Create simple date column where time is not included
+Precip$date <- as.Date(Precip$Date_Time)
 
-Precip$date <- as.Date(Precip1$Date_Time)
+Precip1 <- as.numeric(as.character(Precip$Precipitation)) #creates vector with precipitation values
 
+Precip$seg <- rep(1:7, each= 14)
 aggregate(Precip$Precipitation, by=list(Precip$date), sum)
 
