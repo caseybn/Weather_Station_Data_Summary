4ae1647676844ee3ac39e024821d70b882d4a155
Casey <bnc0015@mix.wvu.edu>
2018-12-05 11:46:57 -0500

    more function head ache

--- CR6_Function.R
+++ CR6_Function.R
@@ -1,11 +1,20 @@
-CR6_Function <- function(climate_variable){
-    if (climate_variable == "Precip") {
-      names(climate_sum)[colnames(climate_sum)=="V3"] <- "Precipitation"
-      climate_sum$Precipitation <- as.numeric(as.character(climate_sum$Precipitation))
-      return(climate_sum)
-    } else {
-      print("not")
+CR6_Function <- function(climate_sum){
+    if (climate_sum == "Precip.csv") {
+      names(climate_sum)[colnames(climate_sum)=="V3"] <- "Precip"
+      climate_sum$Precip <- as.numeric(as.character(climate_sum$Precip))
+    } else if(climate_sum == "Air.csv"){
+      names(climate_sum)[colnames(climate_sum)=="V3"] <- "Temp"
+      names(climate_sum)[colnames(climate_sum)=="V3"] <- "RH"
+      climate_sum$Temp <- as.numeric(as.character(climate_sum$Temp))
+      climate_sum$RH <- as.numeric(as.character(climate_sum$RH)) 
+    } else if(climate_sum == "Sun.csv"){
+      names(climate_sum)[colnames(climate_sum)=="V8"] <- "Solar"
+      climate_sum$Solar <- as.numeric(as.character(climate_sum$Solar))
+    } else if (climate_sum == "Wind.csv"){
+      names(climate_sum)[colnames(climate_sum)=="V3"] <- "WS"
+      climate_sum$WS <- as.numeric(as.character(climate_sum$WS))
     }
+  return(climate_sum)
   }
 
     
--- weather_station_summary.R
+++ weather_station_summary.R
@@ -3,13 +3,16 @@ rm(list=ls())
 
 #Create a vector of files to be summarized
 files <- list.files("./DATA")
-
-for (file_name in files){
-  climate_sum <- read.csv(paste0("DATA/", file_name), header= FALSE, skip = 4)
-  names(climate_sum)[colnames(climate_sum)=="V1"] <- "Date_Time"
-  assign(paste0(file_name, "_summary"), climate_sum)
-  }
-
+file_path_sans_ext
 
 source("CR6_Function.R")
+
+for (file_name in files){
+  climate_sum <- read.csv2(paste0("DATA/", file_name), header= FALSE, skip = 4)
+  names(climate_sum)[colnames(climate_sum)=="V1"] <- "Date_Time"
+  assign(paste0(file_name), climate_sum)
+  matrix_list <- ls(pattern="summary") #creates a vector of all matrices ran through the recoding loop
+  }
+
+climate_sum2 <- apply(climate_sum, 1, CR6_Function)
 
