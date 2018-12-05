fa81d6b6789d22394ca6da56cc30008931086c1e
Brittany Casey <bnc0015@mail.wvu.edu>
2018-12-05 12:19:58 -0500

    THE FUNCTION WORKS!!! I want to cry.

--- CR6_Function.R
+++ CR6_Function.R
@@ -1,16 +1,18 @@
-CR6_Function <- function(climate_sum){
-    if (climate_sum == "Precip.csv") {
+CR6_Function <- function(climate_var){
+  climate_sum <- read.csv2(file= paste0("DATA/", climate_var), header = FALSE, sep = ",", skip = 4)
+  names(climate_sum)[colnames(climate_sum)=="V1"] <- "Date_Time"
+    if (climate_var == "Precip.csv") {
       names(climate_sum)[colnames(climate_sum)=="V3"] <- "Precip"
       climate_sum$Precip <- as.numeric(as.character(climate_sum$Precip))
-    } else if(climate_sum == "Air.csv"){
+    } else if(climate_var == "Air"){
       names(climate_sum)[colnames(climate_sum)=="V3"] <- "Temp"
       names(climate_sum)[colnames(climate_sum)=="V3"] <- "RH"
       climate_sum$Temp <- as.numeric(as.character(climate_sum$Temp))
       climate_sum$RH <- as.numeric(as.character(climate_sum$RH)) 
-    } else if(climate_sum == "Sun.csv"){
+    } else if(climate_var == "Sun"){
       names(climate_sum)[colnames(climate_sum)=="V8"] <- "Solar"
       climate_sum$Solar <- as.numeric(as.character(climate_sum$Solar))
-    } else if (climate_sum == "Wind.csv"){
+    } else if (climate_var == "Wind"){
       names(climate_sum)[colnames(climate_sum)=="V3"] <- "WS"
       climate_sum$WS <- as.numeric(as.character(climate_sum$WS))
     }
