2c8faff93beef243048a37956e3e2da1a5f7e640
Amy Hessl <amy.hessl@mail.wvu.edu>
2018-12-15 16:01:25 -0500

    Well done.  You pushed through a lot of challenges on this project.  I like where you are going with RShiny. Your function works, but could repeat quite a bit less by making the if/else over a list of total vs sum file types, instead of each filename.  Col names could be assigned at the end. It's a great start though and I expect you will continue to refine this.

--- Functions/CR6_Function.R
+++ Functions/CR6_Function.R
@@ -2,6 +2,7 @@ my_station_function <- function(climate_var){
   climate_sum <- read.csv2(file= paste0("DATA/Raw/", climate_var), header = FALSE, sep = ",", skip = 4)
   names(climate_sum)[colnames(climate_sum)=="V1"] <- "Date_Time"
   climate_sum$Date <- as.Date(climate_sum$Date_Time)
+  
   if (climate_var == "Precip.csv") {
     names(climate_sum)[colnames(climate_sum)=="V3"] <- "Precip"
     climate_sum$Precip <- as.numeric(as.character(climate_sum$Precip))
@@ -12,6 +13,7 @@ my_station_function <- function(climate_var){
     by_biweek <- by_day %>% group_by(biwe) %>% summarise(p_sum_mm = sum(p_sum))
     write.csv(by_biweek, file = "DATA/Sum/Precip_biweekly_sums.csv")
     assign("Precip_daily", by_day[,-3], envir = .GlobalEnv)
+    
   } else if(climate_var == "Air.csv"){
     names(climate_sum)[colnames(climate_sum)=="V3"] <- "Temp"
     climate_sum$Temp <- as.numeric(as.character(climate_sum$Temp))
@@ -22,6 +24,7 @@ my_station_function <- function(climate_var){
     by_biweek <- by_day %>% group_by(biwe) %>% summarise(T_mean_C = mean(T_mean))
     write.csv(by_biweek, file = "DATA/Sum/Temp_biweekly_mean.csv")
     assign("Temp_daily", by_day[,-3], envir = .GlobalEnv)
+    
   } else if(climate_var == "Air2.csv"){
     names(climate_sum)[colnames(climate_sum)=="V3"] <- "RH"
     climate_sum$RH <- as.numeric(as.character(climate_sum$RH))
@@ -32,6 +35,7 @@ my_station_function <- function(climate_var){
     by_biweek <- by_day %>% group_by(biwe) %>% summarise(RH_mean = mean(RH_mean))
     write.csv(by_biweek, file = "DATA/Sum/RH_biweekly_mean.csv")
     assign("RH_daily", by_day[,-3], envir = .GlobalEnv)
+    
   } else if(climate_var == "Sun.csv"){
     names(climate_sum)[colnames(climate_sum)=="V8"] <- "Solar"
     climate_sum$Solar <- as.numeric(as.character(climate_sum$Solar))
@@ -42,6 +46,7 @@ my_station_function <- function(climate_var){
     by_biweek <- by_day %>% group_by(biwe) %>% summarise(s_sum = sum(s_sum))
     write.csv(by_biweek, file = "DATA/Sum/Solar_biweekly_Sums.csv")
     assign("Sun_daily", by_day[,-3], envir = .GlobalEnv)
+    
   } else if (climate_var == "Wind.csv"){
     names(climate_sum)[colnames(climate_sum)=="V3"] <- "WS"
     climate_sum$WS <- as.numeric(as.character(climate_sum$WS))
--- Scripts/weather_station_summary.R
+++ Scripts/weather_station_summary.R
@@ -12,13 +12,19 @@ files <- list.files("./DATA/Raw")
 
 #Sets source for function
 source("Functions/CR6_Function.R")
+# Function works well.  A more elegant function would have less repeated.  Everything should be the 
+# same except for the sum vs totals so the rest is repeat. If,else could operate on list of data
+# to be summed vs list to be averaged, then everything else is applied or looped. Col names can be
+# added at the end of each if/else. That would keep the repeats to 2.
 
+#MAKE SURE YOU HAVE A DIRECTORY /DATA/Sum
 
 #applies function across list of desired files 
 mylist <- lapply(files, my_station_function)
 
 #binds the colums from the function output into one dataframe and excludes repeated date columsn
 graph <- do.call(cbind, mylist)
+#Instead of next line, you could adjust what you cbind on above to exclude date
 graph <- graph[(3:65), -c(3,5,7,9)]
 
 
